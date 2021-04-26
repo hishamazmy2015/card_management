eb6fa45ced574d4412786f1143ce9edf5c58f1c3
Hesham Azmy <viji@tpconnects.com>
2021-04-27 02:05:06 +0400

    handle service card

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { MySharedService } from './../../shared/MySharedService.service';
+import { Component, OnInit, OnDestroy, Input, Renderer2 } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
 
@@ -11,12 +12,19 @@ import { RecipeService } from '../recipe.service';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit, OnDestroy {
+  @Input() products: any = [];
+  private singleProduct;
+  private isAdded;
+  
   recipes: Recipe[];
   subscription: Subscription;
 
   constructor(private recipeService: RecipeService,
               private router: Router,
-              private route: ActivatedRoute) {
+              private route: ActivatedRoute,
+              private renderer: Renderer2,
+              /*private mySharedService: MySharedService*/
+              ) {
   }
 
   ngOnInit() {
@@ -27,6 +35,21 @@ export class RecipeListComponent implements OnInit, OnDestroy {
         }
       );
     this.recipes = this.recipeService.getRecipes();
+    this.isAdded = new Array(this.products.length);
+    this.isAdded.fill(false, 0, this.products.length);
+    console.log('this.isAdded -> ', this.isAdded, this.products);
+
+    // this.mySharedService.getProducts().subscribe(data => {
+
+    //   if (data && data.length > 0) {
+
+    //   } else {
+    //     this.products.map((item, index) => {
+    //       this.isAdded[index] = false;
+    //     });
+    //   }
+
+    // });
   }
 
   onNewRecipe() {
@@ -36,4 +59,32 @@ export class RecipeListComponent implements OnInit, OnDestroy {
   ngOnDestroy() {
     this.subscription.unsubscribe();
   }
+
+  // Add item in cart on Button click
+  // ===============================
+
+  addToCart(event, productId) {
+    
+    // If Item is already added then display alert message
+    if (event.target.classList.contains('btn-success')) {
+      alert('This product is already added into cart.');
+      return false;
+    }
+
+    // Change button color to green
+    this.products.map((item, index) => {
+      if (item.id === productId) {
+        this.isAdded[index] = true;
+      }
+    })
+
+    this.singleProduct = this.products.filter(product => {
+      return product.id === productId;
+    });
+
+    // this.cartItems.push(this.singleProduct[0]);
+
+    // this.mySharedService.addProductToCart(this.singleProduct[0]);
+  }
+  
 }
